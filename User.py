cd218e45712aa8b91cd13a8f5eebdaff2464e24d
YOUR_NAME <YOUR_EMAIL>
2025-11-15 09:05:27 -0800

    Finished code with help from insturctor on relative code, and expmale of class Dog

--- User.py
+++ User.py
@@ -1,1 +1,21 @@
 # your User class goes here
+class User: 
+
+    def __init__(self, email_address, driver_licence_number):
+        self.name = email_address
+        self.driver_licence_number = driver_licence_number
+
+    def new_user(self):
+        print(f"Hello {self.name}")
+
+    def show_licence_number(self):
+        print(f"Here is your Dirver Licence Number: {self.driver_licence_number}")
+
+
+
+peach = User("peach@nintendo.com", "DTF987654")
+# Mario = User("mariorules@nintendo.com", "DTF963852")
+# Peach.new_user()
+peach.show_licence_number()
+# Mario.new_user()
+# Mario.show_licence_number("DTF963852")
